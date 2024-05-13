27503f0e972a223e59c156cb1c3da7b3bda5428c
felipe <bello2001felipe@gmail.com>
2024-05-13 16:19:36 +0200

    dependencies specified

--- src/get_data.py
+++ src/get_data.py
@@ -25,20 +25,20 @@ def main():
 
 
     # Code to fetch and save data
-    train = [line.strip() for line in open(train_data, "r").readlines()[1:10000]]
+    train = [line.strip() for line in open(train_data, "r").readlines()[1:]]
 
     raw_x_train = [line.split("\t")[1] for line in train]
     raw_y_train = [line.split("\t")[0] for line in train]
     dump(raw_x_train, 'output/raw_x_train.joblib')
     dump(raw_y_train, 'output/raw_y_train.joblib')
 
-    test = [line.strip() for line in open(test_data, "r").readlines()[:2000]]
+    test = [line.strip() for line in open(test_data, "r").readlines()]
     raw_x_test = [line.split("\t")[1] for line in test]
     raw_y_test = [line.split("\t")[0] for line in test]
     dump(raw_x_test, 'output/raw_x_test.joblib')
     dump(raw_y_test, 'output/raw_y_test.joblib')
 
-    val=[line.strip() for line in open(val_data, "r").readlines()[:2000]]
+    val=[line.strip() for line in open(val_data, "r").readlines()]
     raw_x_val=[line.split("\t")[1] for line in val]
     raw_y_val=[line.split("\t")[0] for line in val]
     dump(raw_x_val, 'output/raw_x_val.joblib')
